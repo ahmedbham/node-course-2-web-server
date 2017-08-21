45bd2fe4eaff1eca64db899e5981046d57bb7fed
Ahmed Bham <ahmed.bham@gmail.com>
2017-08-21 10:02:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+var app = express();
+hbs.registerPartials(__dirname + '\\views\\partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+var now = new Date().toString()
+var log = `${now}: ${req.method} ${req.url}`
+console.log(log)
+fs.appendFile('server.log', log + '\n', (err) => {
+  if (err) { console.log('unable to do it')}
+})
+next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname + '\\public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return 'test' //return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+//  res.send('<h1>Hello Express!</h1>');
+res.render('welcome.hbs', {
+  welcomeMessage: 'Welcome Home',
+  pageTitle: 'About Page',
+});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to do it'
+  });
+});
+
+app.listen('3000', () => {
+  console.log('app is running at post 3000');
+});
