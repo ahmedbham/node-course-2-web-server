05d5721d448c8deadf406d578b7c636e4c7d2331
Ahmed Bham <ahmed.bham@gmail.com>
2017-08-21 13:23:40 -0400

    removing fs code from server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs')
+//const fs = require('fs')
 
 const port = process.env.PORT || 3000
 
@@ -13,9 +13,9 @@ app.use((req, res, next) => {
 var now = new Date().toString()
 var log = `${now}: ${req.method} ${req.url}`
 console.log(log)
-fs.appendFile('server.log', log + '\n', (err) => {
-  if (err) { console.log('unable to do it')}
-})
+// fs.appendFile('server.log', log + '\n', (err) => {
+//   if (err) { console.log('unable to do it')}
+// })
 next();
 });
 
