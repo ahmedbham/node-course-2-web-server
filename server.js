f3d8c8810723415492ba842e08f6452920b1751d
Ahmed Bham <ahmed.bham@gmail.com>
2017-08-23 07:52:15 -0400

    fixed path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 var app = express();
-hbs.registerPartials(__dirname + '\\views\\partials');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
@@ -23,7 +23,7 @@ next();
 //   res.render('maintenance.hbs')
 // });
 
-app.use(express.static(__dirname + '\\public'));
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
